9525c9907b8d0595d35ba3ec288194ee786df0fd
Francis Kodama <franciskodama@Franciss-MacBook-Pro.local>
2022-08-13 11:21:53 -0300

    Included npmrc in gitignore - alerted by git guardian

--- web/frontend/pages/index.jsx
+++ web/frontend/pages/index.jsx
@@ -7,6 +7,7 @@ import {
   Stack,
   Link,
   Heading,
+  Button,
 } from "@shopify/polaris";
 import { TitleBar } from "@shopify/app-bridge-react";
 
@@ -17,64 +18,13 @@ import { ProductsCard } from "../components";
 export default function HomePage() {
   return (
     <Page narrowWidth>
-      <TitleBar title="App name" primaryAction={null} />
+      <TitleBar title="The Lnk Example" primaryAction={null} />
       <Layout>
         <Layout.Section>
           <Card sectioned>
-            <Stack
-              wrap={false}
-              spacing="extraTight"
-              distribution="trailing"
-              alignment="center"
-            >
-              <Stack.Item fill>
-                <TextContainer spacing="loose">
-                  <Heading>Nice work on building a Shopify app 🎉</Heading>
-                  <p>
-                    Your app is ready to explore! It contains everything you
-                    need to get started including the{" "}
-                    <Link url="https://polaris.shopify.com/" external>
-                      Polaris design system
-                    </Link>
-                    ,{" "}
-                    <Link url="https://shopify.dev/api/admin-graphql" external>
-                      Shopify Admin API
-                    </Link>
-                    , and{" "}
-                    <Link
-                      url="https://shopify.dev/apps/tools/app-bridge"
-                      external
-                    >
-                      App Bridge
-                    </Link>{" "}
-                    UI library and components.
-                  </p>
-                  <p>
-                    Ready to go? Start populating your app with some sample
-                    products to view and test in your store.{" "}
-                  </p>
-                  <p>
-                    Learn more about building out your app in{" "}
-                    <Link
-                      url="https://shopify.dev/apps/getting-started/add-functionality"
-                      external
-                    >
-                      this Shopify tutorial
-                    </Link>{" "}
-                    📚{" "}
-                  </p>
-                </TextContainer>
-              </Stack.Item>
-              <Stack.Item>
-                <div style={{ padding: "0 20px" }}>
-                  <Image
-                    source={trophyImage}
-                    alt="Nice work on building a Shopify app"
-                    width={120}
-                  />
-                </div>
-              </Stack.Item>
-            </Stack>
+
+            <Heading>TEST TEST HI </Heading>
+            <Button>Click here!</Button>
           </Card>
         </Layout.Section>
         <Layout.Section>
@@ -84,3 +34,60 @@ export default function HomePage() {
     </Page>
   );
 }
+
+
+
+// <Stack
+// wrap={false}
+// spacing="extraTight"
+// distribution="trailing"
+// alignment="center"
+// >
+// <Stack.Item fill>
+//   <TextContainer spacing="loose">
+//     <Heading>Nice work on building a Shopify app 🎉</Heading>
+//     <p>
+//       Your app is ready to explore! It contains everything you
+//       need to get started including the{" "}
+//       <Link url="https://polaris.shopify.com/" external>
+//         Polaris design system
+//       </Link>
+//       ,{" "}
+//       <Link url="https://shopify.dev/api/admin-graphql" external>
+//         Shopify Admin API
+//       </Link>
+//       , and{" "}
+//       <Link
+//         url="https://shopify.dev/apps/tools/app-bridge"
+//         external
+//       >
+//         App Bridge
+//       </Link>{" "}
+//       UI library and components.
+//     </p>
+//     <p>
+//       Ready to go? Start populating your app with some sample
+//       products to view and test in your store.{" "}
+//     </p>
+//     <p>
+//       Learn more about building out your app in{" "}
+//       <Link
+//         url="https://shopify.dev/apps/getting-started/add-functionality"
+//         external
+//       >
+//         this Shopify tutorial
+//       </Link>{" "}
+//       📚{" "}
+//     </p>
+//   </TextContainer>
+// </Stack.Item>
+// <Stack.Item>
+//   <div style={{ padding: "0 20px" }}>
+//     <Image
+//       source={trophyImage}
+//       alt="Nice work on building a Shopify app"
+//       width={120}
+//     />
+//   </div>
+// </Stack.Item>
+// </Stack>
